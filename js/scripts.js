33f350304feaa526038ab5913d81da894484ede3
Kyle Wolfson <wolfsonk@gmail.com>
2016-03-25 09:34:18 -0700

    Added input form and main page logic with nested if statements, probably should have used a combination of and statements and nesting to make the code slightly more readable.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document)ready(function() {
+  $("form#destinationChoice").submit(function(event)) {
+    event.preventDefault();
+    var travel = $("#travel").val();
+    var geography = $("#geography").val();
+    var density = $("#density").val();
+    var price = $("#price").val();
+    var temperature = $("#temperature").val();
+
+    // begin excessive nested if statements
+
+    
+  }
+})
