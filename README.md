ee5b8d68155dd5c978aeee3a023908c002d0d44e
Kyle Wolfson <wolfsonk@gmail.com>
2016-03-25 09:52:03 -0700

    have a README

---
+++ README.md
@@ -0,0 +1,34 @@
+# _{Vacation picker Webpage}_
+
+#### _{User answers questions and gets a suggested vacation destination}, {March 25, 2016}_
+
+#### By _**{Kyle Wolfson}**_
+
+## Description
+
+_{Uses nested if statements to choose a vacation spot based on user inputs}_
+
+## Setup/Installation Requirements
+
+*  _Just a webpage, load it up!_
+
+
+_{Any modern webpage should load just fine with Javascript compatibility.}_
+
+## Known Bugs
+
+_{Bug free!}_
+
+## Support and contact details
+
+_{email me at wolfsonk@gmail.com with any feedback}_
+
+## Technologies Used
+
+_{JavaScript as well as HTML and CSS}_
+
+### License
+
+*{No license}*
+
+Copyright (c) 2016 **_{Kyle Wolfson}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,68 @@
-$(document)ready(function() {
-  $("form#destinationChoice").submit(function(event)) {
+$(document).ready(function() {
+  $("form#destinationChoice").submit(function(event) {
     event.preventDefault();
     var travel = $("#travel").val();
     var geography = $("#geography").val();
     var density = $("#density").val();
     var price = $("#price").val();
     var temperature = $("#temperature").val();
-
+    var result
     // begin excessive nested if statements
 
-    
-  }
-})
+    if (travel === "domestic") {
+    $("#domesticResult").show()
+    $("#foreignResult").hide()
+      if (geography === "water") {
+        if (density === "city") {
+          if (price === "cheap") {
+            if (temperature === "hot") {result = "Tallahassee" }
+            else {result = "Buffalo"}
+          }
+          else { //classy
+            if (temperature === "hot") {result = "Los Angeles" }
+            else {result = "New York"}
+          }
+        }
+
+        else { //rural
+          if (price === "cheap") {
+            if (temperature === "hot") {result = "the Everglades" }
+            else {result = "the Allagash Wilderness"}
+          }
+          else { //classy
+            if (temperature === "hot") {result = "a remote Florida key to go jetskiing" }
+            else {result = "the Oregon coast for drysuit diving"}
+          }
+        }
+      }
+      else { //mountain
+        if (density === "city") {
+          if (price === "cheap") {
+            if (temperature === "hot") {result = "Phoenix" }
+            else {result = "Salt Lake City"}
+          }
+          else { //classy
+            if (temperature === "hot") {result = "Las Vegas" } //Turns out Googling "classy hot mountain city" gives some unexpected results.
+            else {result = "Denver"}
+          }
+        }
+        else { //rural
+          if (price === "cheap") {
+            if (temperature === "hot") {result = "Guadalupe National Park" }
+            else {result = "Mount Hood"}
+          }
+          else { //classy
+            if (temperature === "hot") {result = "a guided tour of the Grand Canyon" }
+            else {result = "Aspen for a snowmobile adventure"}
+          }
+        }
+      }
+    }
+    else {  //foreign travel
+      $("#domesticResult").hide()
+      $("#foreignResult").show()
+    }
+
+    $("#output").text(result);
+  });
+});
